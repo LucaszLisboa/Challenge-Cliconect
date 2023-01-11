71ad38ae8b60c46209f58aa1c8fb0ef5302c0c6e
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2023-01-11 18:06:45 -0300

    listagem de pacientes de forma paginada

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,22 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { TableComponent } from './table/table.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularMaterialModule } from "./material.module"
+import { AngularMaterialModule } from "./material.module";
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation'
 
 
 @NgModule({
@@ -28,7 +43,22 @@ import { AngularMaterialModule } from "./material.module"
     HttpClientModule,
     ReactiveFormsModule,
     AngularMaterialModule,
-    CommonModule
+    CommonModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/table/table.component.ts
+++ frontend/src/app/table/table.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
 import { PacienteService } from 'src/app/services/paciente.service';
 import { Paciente } from 'src/app/models/paciente';
 import { NgForm } from '@angular/forms';
-import { tap } from 'rxjs';
+import { NEVER, tap } from 'rxjs';
+import { MdbTableDirective } from 'angular-bootstrap-md';
 
 @Component({
   selector: 'app-table',
@@ -15,33 +17,56 @@ import { tap } from 'rxjs';
 export class TableComponent implements OnInit, AfterViewInit {
 
   paciente = {} as Paciente;
-  pacientes: Paciente[];
+  pacientes: Paciente[] = [];
+
+  pageSlice: any;
+
+  displayedColumns: string[] = ['id', 'nome','sexo', 'cpf', 'celular', 'email', 'dataNascimento','informacoesAtendimento','rua',
+  'numero', 'bairro', 'cidade', 'estado'];
+  dataSource: MatTableDataSource<Paciente>;
 
   @ViewChild(MatPaginator) 
   paginator: MatPaginator; 
-  
 
+  @ViewChild(MatSort)
+  sort!: MatSort;
+  
   constructor(
     private pacienteService: PacienteService
-  ) { }
+  ) { 
+    
+  }
+
+  onPageChange(event: PageEvent) {
+    console.log(event);
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.pacientes.length) {
+      endIndex = this.pacientes.length;
+    }
+    
+    this.pageSlice = this.pacientes.slice(startIndex, endIndex);
+  } 
   
   ngAfterViewInit(): void {
-    this.paginator.page
-      .pipe(
-        tap(() => this.getPacientes())
-      )
-      .subscribe();
+    this.dataSource = new MatTableDataSource(this.pacientes);
+    this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
     
+
+
     this.getPacientes();
 
-    this.pacienteService.getPacientes().subscribe(
-      (data) => {
-        console.log(data);
-      },
-    );
+    // this.pacienteService.getPacientes().subscribe(
+    //   (data) => {
+    //     console.log(data);
+    //     this.dataSource = new MatTableDataSource(data);
+    //     this.dataSource.paginator = this.paginator;
+    //     this.dataSource.sort = this.sort;
+    //   },
+    // );
   
   }
 
@@ -60,12 +85,10 @@ export class TableComponent implements OnInit, AfterViewInit {
 
   getPacientes(){
     this.pacienteService.getPacientes()
-      // .pipe(
-      //   tap(() => this.getPacientes())
-      // )
       .subscribe((pacientes: Paciente[]) => {
         this.pacientes = pacientes;
-      });
+        this.pageSlice = this.pacientes.slice(0, 5);
+    });
   }
 
   deletePaciente(paciente: Paciente){
@@ -87,6 +110,16 @@ export class TableComponent implements OnInit, AfterViewInit {
   handlePageEvent(event: any){
     console.log(event);
   }
+
+  // search(event: Event): void {
+  //   const searchTerm = (event.target as HTMLInputElement).value;
+  //   this.table.searchDataObservable(searchTerm);
+  // }
+
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
 
 
